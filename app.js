8f691cf59766f025fded1f5f79c3ef1b60ddd223
shiela-fajardo <134486354+shiela-fajardo@users.noreply.github.com>
2023-10-06 23:19:47 +0800

    unit2.5

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-console.log("test message");
-console.log("this is executed in node"); //inline comment
-/* multiple
- * line 
- * comments 
+/**
+ * variables and data types
+ */
+
+var a = 10;
+var b = 12;
+var c = a + b;
+console.log("a = "+a); //like concatenate; treats as string
+console.log("b = "+b);
+console.log("a + b = "+c);
+console.log("9"+1+7); //output=917; all treated as string
+console.log(1+8+"4"); //output=94;
+/*1st command 1+8 treated as num; 
+then concats because 4 is treated as string
 */
