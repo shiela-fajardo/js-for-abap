3b4134701cc647ccd14d8354d0e493982eaea2a5
shiela-fajardo <134486354+shiela-fajardo@users.noreply.github.com>
2023-10-07 22:32:24 +0800

    unit2.4

--- app.js
+++ app.js
@@ -1,22 +1,49 @@
 /**
+ * unit 2.3 scope of global & local var
+ */
+var car = "BMW"; //global scope
+function showCar(){
+    console.log(car); //car is accessible bec it's global var
+}
+
+showCar(); //BMW
+
+// function localScope(){
+//     var car1 = 'Toyota'; //local var
+//     console.log(car1);
+// }
+
+// localScope(); //BMW
+// console.log(car1); //ReferenceError: car1 is not defined
+
+function blockScope(){
+    if (true){
+        var car2 = "Audi"; //old syntax in function scope
+        let car3 = "Ferrari"; //modern syntax in block scope
+    }
+    console.log(car2);//Audi
+    console.log(car3);//ReferenceError: car2 is not defined
+}
+blockScope();
+
+/**
  * unit 2.2 data types
  */
-
-var car; //undefined primitive type -->empty var
-console.log(car); //undefined
-console.log(typeof car); //undefined
-var weightKg = 10; //num primitive type
-console.log(typeof weightKg);
-var dogsName = "Spot"; //string primitive type
-console.log(typeof dogsName);
-var isTaskComplete = false; //boolean primitive type
-console.log(typeof isTaskComplete);
-var numArray = [1,2,3,4,5]; //object structural type
-console.log(typeof numArray);
-var object = { firstName: "John", lastName: "Smith"}; //structural type
-console.log(typeof object);
-var fruit = 42 + 8 + "apple";
-console.log(fruit); // 50apple
+// var car; //undefined primitive type -->empty var
+// console.log(car); //undefined
+// console.log(typeof car); //undefined
+// var weightKg = 10; //num primitive type
+// console.log(typeof weightKg);
+// var dogsName = "Spot"; //string primitive type
+// console.log(typeof dogsName);
+// var isTaskComplete = false; //boolean primitive type
+// console.log(typeof isTaskComplete);
+// var numArray = [1,2,3,4,5]; //object structural type
+// console.log(typeof numArray);
+// var object = { firstName: "John", lastName: "Smith"}; //structural type
+// console.log(typeof object);
+// var fruit = 42 + 8 + "apple";
+// console.log(fruit); // 50apple
 
 /**
  * unit 1 variables and data types
